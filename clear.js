9f2a62d2f215559eb1398e9fd38adbc064f16719
Augustin Godiscal <dev@auggod.io>
2017-07-14 18:06:04 +0200

    Check if navigator.serviceWorker is also present

--- clear.js
+++ clear.js
@@ -3,7 +3,7 @@ module.exports = clear
 function clear () {
   return function (state, emitter) {
     emitter.prependListener(state.events.DOMCONTENTLOADED, function () {
-      if (!navigator.onLine) return
+      if (!navigator.onLine || !navigator.serviceWorker) return
       var registrations = navigator.serviceWorker.getRegistrations()
       registrations.then(function (registrations) {
         Object.keys(registrations).forEach(function (key) {
